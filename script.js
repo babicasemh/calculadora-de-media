c7187414814658ab6f48dcd39851a8161f630a7e
babica <babicasemh@gmail.com>
2026-03-04 11:24:06 -0300

    pegamos os dados

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert ("deu certo")
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+
+const button = document.getElementById("button")
+
+function calcularMedia(){
+    const valorNota1 = number(nota1.value)
+    const valorNota2 = number(nota2.value)
+
+    const media = (valorNota1 + valorNota2)/2
+
+    console.log(media)
+}
+
+button.addEventLister("click", calcularMedia)
