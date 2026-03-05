3f8deda0db2f37a7d44377388c09b611d0290e74
babica <babicasemh@gmail.com>
2026-03-05 07:53:35 -0300

    finalizamos

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const nota1 = document.getElementById("nota1")
 const nota2 = document.getElementById("nota2")
+const resultado = document.getElementById("resultado")
 
 const button = document.getElementById("button")
 
 function calcularMedia(){
-    const valorNota1 = number(nota1.value)
-    const valorNota2 = number(nota2.value)
+    const valorNota1 = Number(nota1.value)
+    const valorNota2 = Number(nota2.value)
 
     const media = (valorNota1 + valorNota2)/2
 
-    console.log(media)
+    resultado.innerText = `A media e ${media.toFixed(5)}`
 }
 
 button.addEventLister("click", calcularMedia)
